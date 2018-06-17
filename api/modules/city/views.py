c36a3ebd611f710cfab33c1374d229941973fdb8
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2018-06-17 10:41:32 +0530

    Add response codes in docstrings for all views
    Add missing documentation for all the views

--- api/modules/city/views.py
+++ api/modules/city/views.py
@@ -16,12 +16,11 @@ def get_all_cities(request, no_of_cities=8):
     Returns a list of all the cities
     :param request:
     :param no_of_cities: (default count: 8)
-    :return:
+    :return: 200 successful
     """
-    if request.method == 'GET':
-        cities = City.objects.all().order_by('-id')[:no_of_cities]
-        serializer = CitySerializer(cities, many=True)
-        return Response(serializer.data)
+    cities = City.objects.all().order_by('-id')[:no_of_cities]
+    serializer = CitySerializer(cities, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -30,16 +29,16 @@ def get_city(request, city_id):
     Returns a city on the basis of city id
     :param request:
     :param city_id:
-    :return:
+    :return: 404 if invalid city id is sent
+    :return: 200 successful
     """
     try:
         city = City.objects.get(pk=city_id)
     except City.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = CitySerializer(city)
-        return Response(serializer.data)
+    serializer = CitySerializer(city)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -48,7 +47,7 @@ def get_city_by_name(request, city_prefix):
     Returns a list of cities that starts with the given city prefix
     :param request:
     :param city_prefix:
-    :return:
+    :return: 200 successful
     """
     cities = City.objects.filter(city_name__istartswith=city_prefix)[:5]
     serializer = CitySerializer(cities, many=True)
@@ -61,16 +60,16 @@ def get_all_city_images(request, city_id):
     Returns a list of all the images for a given city id
     :param request:
     :param city_id:
-    :return:
+    :return: 404 if invalid city id is sent
+    :return: 200 successful
     """
     try:
         city_images = CityImage.objects.filter(city=city_id)
     except CityImage.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = CityImageSerializer(city_images, many=True)
-        return Response(serializer.data)
+    serializer = CityImageSerializer(city_images, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -79,16 +78,16 @@ def get_all_city_facts(request, city_id):
     Returns a list of all the facts for a given city id
     :param request:
     :param city_id:
-    :return:
+    :return: 404 if invalid city id is sent
+    :return: 200 successful
     """
     try:
         city_facts = CityFact.objects.filter(city=city_id)
     except CityFact.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = CityFactSerializer(city_facts, many=True)
-        return Response(serializer.data)
+    serializer = CityFactSerializer(city_facts, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -97,7 +96,9 @@ def get_city_trends(request, city_id):
     Returns a list of top trending tweets in the given city
     :param request:
     :param city_id:
-    :return:
+    :return: 404 if invalid city id is sent
+    :return: 503 if Twitter API request fails
+    :return: 200 successful
     """
     try:
         city = City.objects.get(pk=city_id)
