92a5dcc2ce008c16ba8bc64677f5b632e0f9ae42
Mohit Khandelwal <mohitkh7@gmail.com>
2018-06-24 20:43:27 +0530

    add type field in feedback, fixes #26

--- api/modules/feedback/model.py
+++ api/modules/feedback/model.py
@@ -4,5 +4,6 @@ from django.db import models
 
 class Feedback(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    type = models.CharField(max_length=255, default="Other")
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
