b0b6bf0f4a4df96b15d425a31e1eae4ea0b4dca8
arunikayadav42 <arunikayadav42@gmail.com>
2018-06-27 03:04:50 +0530

    Added a missing comma in the group of app names in installed_apps in the settings.py file in the nomad directory

--- nomad/settings.py
+++ nomad/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
