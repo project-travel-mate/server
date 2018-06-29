588abc489a462abb73ea1e5d21e4192878182078
Mohit Khandelwal <mohitkh7@gmail.com>
2018-06-30 03:42:48 +0530

    fix #1, throttle requests

--- nomad/settings.py
+++ nomad/settings.py
@@ -147,7 +147,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/hour',
+        'user': '1500/hour',
+    }
 }
 
 LOGGING = {
