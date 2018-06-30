bc5bd62a5932057b795a9b1bc612d7cc877ba8f3
Pulkit gera <pulkithifi@yahoo.com>
2018-06-30 07:14:39 +0530

    almost through with the first task as well

--- api/modules/feedback/serializers.py
+++ api/modules/feedback/serializers.py
@@ -1,19 +1,7 @@
 from rest_framework import serializers
 from api.models import Feedback
 from api.modules.users.serializers import UserSerializer
-
-
-class FeedbackSerializer(serializers.ModelSerializer):
-
-    #user = UserSerializer(many=False, read_only=True)
-    user = serializers.RelatedField(source='user.id', read_only=True)
-
-
-    class Meta:
-        model = Feedback
-        fields = ('id','user','type','created')
-        read_only=('id','user','type','created')
-
+   
 
 class FeedbackIDSerializer(serializers.ModelSerializer):
 
@@ -21,4 +9,9 @@ class FeedbackIDSerializer(serializers.ModelSerializer):
         model = Feedback
         fields = ('id','text', 'type', 'created')
 
-    
+class FeedbackSerializer(serializers.ModelSerializer):
+    users = UserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Feedback
+        fields = ('id','created','users','type')
