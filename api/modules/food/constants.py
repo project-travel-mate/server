c30731de8cf5773e6d2a1ee95c5664a46ee110e8
Pulkit gera <pulkithifi@yahoo.com>
2018-07-06 03:17:47 +0530

    minor bugs solved

--- api/modules/food/constants.py
+++ api/modules/food/constants.py
@@ -7,5 +7,5 @@ import os
 ORIGINAL_ZOMATO_API = "9a656813579b698902619a05684d39c6"
 ZOMATO_API_KEY = os.environ.get("ZOMATO_API", ORIGINAL_ZOMATO_API)
 BASE_URL = "https://developers.zomato.com/api/v2.1/geocode?lat={0}&lon={1}"
-USER_AGENT: "curl/7.43.0"
-ACCEPT:  "application/json"
+USER_AGENT = "curl/7.43.0"
+ACCEPT =  "application/json"
