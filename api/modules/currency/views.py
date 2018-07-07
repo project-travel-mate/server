95c190d542356d0d8ac73bc0a8f157c267c9b62f
arunikayadav42 <arunikayadav42@gmail.com>
2018-07-07 13:22:04 +0530

    method docs added to the currency api module

--- api/modules/currency/views.py
+++ api/modules/currency/views.py
@@ -8,12 +8,20 @@ from api.modules.currency.currency_item import CurrencyItem
 
 @api_view(['GET'])
 def get_currency_exchange_rate(request, source_currency_code, target_currency_code):
+    """
+    Return currency conversion rate using source and target currency codes
+    :param request:
+    :param source_currency_code:
+    :param target_currency_code:
+    :return: 503 if Free Currency Converter api fails
+    :return: 200 successful
+    """
     query = "{0}_{1}".format(source_currency_code, target_currency_code)
     try:
         api_response = requests.get(CURRENCY_CONVERTER_API_URL.format(query))
         api_response_json = api_response.json()
         if not api_response.ok:
-            error_message = api_response["error"]
+            error_message = "Missing parameters in the api response"
             return Response(error_message, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
         response = CurrencyItem(source=api_response_json["results"][query]["fr"],
