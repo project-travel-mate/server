c7b51315aa8533f058456e19014ad5647da13065
Pulkit gera <pulkithifi@yahoo.com>
2018-08-02 01:20:40 +0530

    Fix #101 Add welcome email functionality for user sign

---
+++ api/modules/email/templates.py
@@ -0,0 +1,16 @@
+# Email template signature
+EMAIL_SIGNATURE = """
+--
+Team Project Travel Mate
+Made open source with ❤️ and coffee at https://github.com/project-travel-mate
+
+Buy us a ☕ to keep us stayed fueled up at https://www.buymeacoffee.com/qITGMWB
+"""
+
+# User welcome mails
+WELCOME_MAIL_SUBJECT = "Welcome {0} to Travel Mate!"
+WELCOME_MAIL_CONTENT = """
+Welcome {0},
+Thank you for registering with us.
+
+""" + EMAIL_SIGNATURE
--- nomad/settings.py
+++ nomad/settings.py
@@ -11,7 +11,15 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+
 import dj_database_url
+
+# Setting up email server
+EMAIL_USE_TLS = True
+DEFAULT_EMAIL_SENDER = "Project Travel Mate <projecttravelmate.github.io@gmail.com>"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_CONFIG_ADDRESS", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_CONFIG_PASSWORD", "")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
