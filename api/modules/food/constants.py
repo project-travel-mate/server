b670e65beae3c36c9c5871a327952863a96ab89c
Pulkit gera <pulkithifi@yahoo.com>
2018-08-03 23:57:48 +0530

    minor isssue solved

--- api/modules/food/constants.py
+++ api/modules/food/constants.py
@@ -6,7 +6,7 @@ Environment variable for API to be set
 import os
 ORIGINAL_ZOMATO_API = ""
 ZOMATO_API_KEY = os.environ.get("ZOMATO_API", ORIGINAL_ZOMATO_API)
-BASE_URL = "geocode?lat={0}&lon={1}"
+BASE_URL = "https://developers.zomato.com/api/v2.1/"
 LIST = "geocode?lat={0}&lon={1}"
 RESTAURANT = "restaurant?res_id={0}"
 USER_AGENT = "curl/7.43.0"
