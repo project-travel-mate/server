e5e411cd70fa2b743e1c32dfe6bc506910aa3db0
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2018-10-10 09:57:56 -0700

    Add active users count in analytics API

--- api/modules/analytics/views.py
+++ api/modules/analytics/views.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta
+
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from django.contrib.auth.models import User
+from api.modules.analytics.constants import NUMBER_OF_DAYS_FOR_ACTIVE_STATUS
+from nomad.settings import TIME_ZONE_SUBCLASS
 
 
 @api_view(['GET'])
@@ -13,7 +17,12 @@ def get_total_users(request):
     :return: 200 successful
     """
     number_of_users = User.objects.count()
+    end_date = datetime.now(tz=TIME_ZONE_SUBCLASS)
+    start_date = end_date - timedelta(days=NUMBER_OF_DAYS_FOR_ACTIVE_STATUS)
+    number_of_active_users = User.objects.filter(profile__last_active__range=[start_date, end_date]).count()
+
     res = {
         'total_users': number_of_users,
+        'active_users': number_of_active_users,
     }
     return Response(res, status=status.HTTP_200_OK)
