25c5754d91ef63a95922809271ade079be1005df
badducoder <sumit7engineer@gmail.com>
2019-06-16 01:06:27 +0530

    Analytics API: Added number of verified users.

--- api/modules/analytics/views.py
+++ api/modules/analytics/views.py
@@ -17,12 +17,16 @@ def get_total_users(request):
     :return: 200 successful
     """
     number_of_users = User.objects.count()
+    number_of_verified_users = User.objects.filter(profile__is_verified=True).count()
     end_date = datetime.now(tz=TIME_ZONE_SUBCLASS)
     start_date = end_date - timedelta(days=NUMBER_OF_DAYS_FOR_ACTIVE_STATUS)
     number_of_active_users = User.objects.filter(profile__last_active__range=[start_date, end_date]).count()
+    number_of_active_verified_users = User.objects.filter(profile__last_active__range=[start_date, end_date], profile__is_verified=True).count()
 
     res = {
         'total_users': number_of_users,
         'active_users': number_of_active_users,
+        'verified_users': number_of_verified_users,
+        'active_verified_users': number_of_active_verified_users
     }
     return Response(res, status=status.HTTP_200_OK)
