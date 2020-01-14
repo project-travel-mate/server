e4919ffde24d7a71de70acf4101d3d5be51fd713
Anjali Sharma <anjali.sh0101@gmail.com>
2020-01-14 15:04:01 +0530

    [Update Redirect Url]

--- nomad/constants.py
+++ nomad/constants.py
@@ -1,4 +1,4 @@
-URL_NOT_FOUND_REDIRECT = 'http://project-travel-mate.github.io/Travel-Mate/'
+URL_NOT_FOUND_REDIRECT = 'http://project-travel-mate.github.io'
 GITHUB_API_URL = 'https://api.github.com'
 GITHUB_REPO_ORG = 'project-travel-mate'
 GITHUB_REPO_NAME = 'server'
